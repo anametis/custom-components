fa18e9cfd3bc536ee3349f89afcbfa0316ccafbc
Ali_S <alisafarpour.1997@gmail.com>
2024-11-01 22:55:18 +0330

    chore(page): Syntax improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,17 @@
-import CircularRings from "@/components/chart/CircularRings";
 import MultiRings from "@/components/chart/MultiRigs";
 import ringData from "@/data/dummy/ringData";
 import Tooltip from "@/components/tooltip/Tooltip";
 import Quotex from "@/components/Quotex";
-import useSocketIo from "@/hooks/useSocketIO";
+// import CircularRings from "@/components/chart/CircularRings";
+// import useSocketIo from "@/hooks/useSocketIO";
 
 export default function Home() {
   return (
     <div className="mx-auto flex flex-col justify-center items-center">
-      <Quotex />
+      {/* <Quotex /> */}
+
       <MultiRings data={ringData} />
 
-
-      {/* <div className="flex w-full items-center justify-start gap-10 flex-wrap ">
-        <Tooltip text="this is 1">
-          <div className="bg-red-500 rounded-full text-center h-10 w-10">1</div>
-        </Tooltip>
-      </div> */}
-      {/* <div className="grid gap-4 grid-cols-1 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-8 w-full">
-        {Array.from({ length: 23 }).map((_, index) => (
-          <div
-            key={index}
-            className="bg-red-500 h-20 w-24 flex items-center justify-center flex-col mx-auto"
-          >
-            {index}
-          </div>
-        ))}
-      </div> */}
       <div className="flex flex-wrap gap-4 justify-between">
         {Array.from({ length: 23 }).map((_, index) => (
           <div
@@ -39,19 +24,13 @@ export default function Home() {
       </div>
       <div
         className="w-full bg-gray-500 grid gap-3 sm:justify-between justify-center"
-        style={{
-          display: "grid",
-          gap: "10px",
-          gridTemplateColumns: "repeat(auto-fit,96px)",
-        }}
+        style={{ gridTemplateColumns: "repeat(auto-fit,96px)" }}
       >
         {Array.from({ length: 23 }).map((_, index) => (
           <Tooltip key={index} text="hi">
-          <div
-            className="bg-yellow-500 h-24 w-24 flex items-center justify-center flex-col"
-          >
-            {index}
-          </div>
+            <div className="bg-yellow-500 h-24 w-24 flex items-center justify-center flex-col">
+              {index}
+            </div>
           </Tooltip>
         ))}
       </div>
